0b8452b5e195d8f7977441b84ebe3e3a81fd5d5c
JavierPs287 <your-email@example.com>
2026-05-05 17:31:47 +0200

    .

--- src/main/java/edu/esi/ds/esiusuarios/config/CorsConfig.java
+++ src/main/java/edu/esi/ds/esiusuarios/config/CorsConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration 
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200","http://localhost:8080") // URL de tu Angular
-                .allowCredentials(false) // Permite el paso de la cookie de sesión
+                .allowCredentials(true) // Permite el paso de la cookie de sesión
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
